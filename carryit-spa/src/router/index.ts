f72345b36b07e77e77f56390aaee9f86db2a2e74
Manoug Pfennig <manoug.pfen@gmail.com>
2026-01-05 11:15:19 +0100

    setup router

---
+++ carryit-spa/src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/Home/Home.vue'
+import AboutPage from '../views/About/About.vue'
+import NotFoundPage from '../views/NotFound/NotFound.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+	{ path: '/', component: HomePage },
+	{ path: '/about', component: AboutPage },
+	{ path: '/:catchAll(.*)', component: NotFoundPage },
+  ],
+})
+
+export default router
---
+++ carryit-spa/src/views/NotFound/NotFound.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>This page doesn't exist.</h1>
+</template>
+
+<style scoped></style>
